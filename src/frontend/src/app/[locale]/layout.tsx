bcf02e3cb1b69a60022b2ec419ecd716dec013fe
PhamHuuLoc <phamhuulocforwork@gmail.com>
2024-10-29 08:40:43 +0700

    hydration mismatch fixed, layout fixed

--- src/frontend/src/app/[locale]/layout.tsx
+++ src/frontend/src/app/[locale]/layout.tsx
@@ -1,14 +1,7 @@
 import type { Metadata } from "next";
-import "./globals.css";
-import { Inter } from "next/font/google";
 
-import { NextIntlClientProvider } from "next-intl";
-import { getMessages } from "next-intl/server";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/SideBar";
 import { AppSidebar } from "@/components/app/AppSideBar";
-import { ThemeProvider } from "next-themes";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "KnowHub",
@@ -17,28 +10,16 @@ export const metadata: Metadata = {
 
 export default async function LocaleLayout({
   children,
-  params: { locale },
 }: Readonly<{
   children: React.ReactNode;
-  params: { locale: string };
 }>) {
-  const messages = await getMessages();
-
   return (
-    <html suppressHydrationWarning lang={locale}>
-      <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="light">
-          <NextIntlClientProvider messages={messages}>
-            <SidebarProvider>
-              <AppSidebar />
-              <main>
-                <SidebarTrigger />
-                {children}
-              </main>
-            </SidebarProvider>
-          </NextIntlClientProvider>
-        </ThemeProvider>
-      </body>
-    </html>
+    <SidebarProvider>
+      <AppSidebar />
+      <main>
+        <SidebarTrigger />
+        {children}
+      </main>
+    </SidebarProvider>
   );
 }
--- src/frontend/src/app/layout.tsx
+++ src/frontend/src/app/layout.tsx
@@ -1,7 +1,36 @@
-export default function RootLayout({
+import type { Metadata } from "next";
+import "./globals.css";
+import { Inter } from "next/font/google";
+
+import { NextIntlClientProvider } from "next-intl";
+import { getMessages } from "next-intl/server";
+import { ThemeProvider } from "next-themes";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "KnowHub",
+  description: "Learn everything!",
+};
+
+export default async function LocaleLayout({
   children,
+  params: { locale },
 }: Readonly<{
   children: React.ReactNode;
+  params: { locale: string };
 }>) {
-  return children;
+  const messages = await getMessages();
+
+  return (
+    <html suppressHydrationWarning lang={locale}>
+      <body className={inter.className}>
+        <ThemeProvider defaultTheme="light" attribute="class">
+          <NextIntlClientProvider messages={messages}>
+            {children}
+          </NextIntlClientProvider>
+        </ThemeProvider>
+      </body>
+    </html>
+  );
 }
